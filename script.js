e573315bfa21733273d515a911e25ec4a923958a
Atthapon <atthapon.fe@gmail.com>
2024-03-22 15:21:42 +0700

    Up 3-1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-                                            //--- แบบตาราง ---//
+//--- แบบตาราง ---//
 // fetch('peopie.json')
 //     .then(function (response) {
 //         return response.json()
@@ -10,14 +10,14 @@
 //         console.log('error: ' + err)
 //     })
 
-                          //------- สามารถเขียนในอีกรูปแบบนึงได้รีกว่า arrow --------//
-    // fetch('peopie.json')
-    //     .then(response => response.json())
-    //     .then(data => appendData(data))
-    //     .catch(err => { console.log('error: ' + err)})
+//------- สามารถเขียนในอีกรูปแบบนึงได้รีกว่า arrow --------//
+// fetch('peopie.json')
+//     .then(response => response.json())
+//     .then(data => appendData(data))
+//     .catch(err => { console.log('error: ' + err)})
 
 // function appendData(data) {
-    
+
 //                                         //------- 1 --------//
 //         var mainContainer = document.getElementById("myData")
 //         for (var i = 0; i < data.length; i++){
@@ -26,8 +26,8 @@
 
 //                                         //---- วิธีเขียนอีกแบบให้มันสั่นลง ----//
 //             // div.innerHTML = `ID: ${data[i].id}  จังหวัด: ${data[i].name}  เมืองหลวง: ${data[i].capital} ขนาดพื้นที่:  ${data[i].area} ประชากร:  ${data[i].population} `
-            
-//             mainContainer.appendChild(div)  
+
+//             mainContainer.appendChild(div)
 //         }
 // }
 //                                         //------- 2 --------//
@@ -36,15 +36,14 @@
 //         // const innerDiv = document.createElement("div");
 //         // innerDiv.classList.add("inner");
 //         // innerDiv.innerHTML = `
-//         //     <p>ID: ${province.id}</p> 
+//         //     <p>ID: ${province.id}</p>
 //         //     <p>จังหวัด: ${province.name}</p>
 //         //     <p>เมืองหลวง: ${province.capital}</p>
 //         //     <p>ขนาดพื้นที่: ${province.area}</p>
 //         //     <p>ประชากร: ${province.population}</p>
 //         //     <hr>
 //         //     `
- 
 
 //   container.appendChild(innerDiv);
 // });
-// }                                     
+// }
